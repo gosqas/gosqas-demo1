f4b58595d91ac94638649ec89e515aaaaea1e773
Harry Pierson <harrypierson@hotmail.com>
2023-06-13 09:24:12 -0700

    no, i really do hate pug

--- app.ts
+++ app.ts
@@ -6,10 +6,10 @@ dotenv.config();
 const port = process.env.PORT || 3000;
 
 const app = express();
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', { title: 'Express GOSQAS Provenence Tracker' });
 });
 
 app.listen(port, () => {
